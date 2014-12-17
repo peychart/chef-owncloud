3cda0706c48ca6423ae8cca06b83b3379759b4ce
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-12-17 13:05:56 -1000

    aother options

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,12 +5,18 @@
 default['chef-owncloud']['datadirectory'] = '/var/www/owncloud/data'
 default['chef-owncloud']['ssl']['enable'] = true
 default['chef-owncloud']['ssl']['force'] = true
-default['chef-owncloud']['proxy'] = ''
 
-default['chef-owncloud']['default_language'] = 'fr'
+default['chef-owncloud']['otheroptions'] = [
+  "'enable_avatars' => true",
+  "",
+  "'appstoreenabled' => false",
+  "'blacklisted_files' => array('.htaccess')"
+]
 
 default['chef-owncloud']['dbtype'] = 'mysql'
-default['chef-owncloud']['dbhost'] = 'localhost'
-default['chef-owncloud']['dbrootPassword'] = 'secret'
+default['chef-owncloud']['dbhost'] = '127.0.0.1'
+default['chef-owncloud']['dbrootpassword'] = 'secret'
 default['chef-owncloud']['dbname'] = 'owncloud'
 default['chef-owncloud']['dbpassword'] = 'secret'
+
+
